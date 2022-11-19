b3b1cc7b878c897cc55a9bb649c11acb72b74d72
Lilly2312 <117816232+Lilly2312@users.noreply.github.com>
2022-11-19 19:48:04 +0530

    Add files via upload

---
+++ 3_even_and_odd.py
@@ -0,0 +1,16 @@
+list1 = [2, 5, 10, 25, 45]
+ 
+even_count, odd_count = 0, 0
+ 
+
+for num in list1:
+ 
+    
+    if num % 2 == 0:
+        even_count += 1
+ 
+    else:
+        odd_count += 1
+ 
+print("Even numbers in the list: ", even_count)
+print("Odd numbers in the list: ", odd_count)
---
+++ _2_reverse.py
@@ -0,0 +1,7 @@
+name = str(input("enter the word "))
+word = name
+reverse = " "
+for i in word:
+	reverse = i + reverse
+
+print(reverse)
